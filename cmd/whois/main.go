60b00e85a6ae47b2da681494b49ff8eadda1fc6f
jgould <jgould@phosphorus.io>
2022-07-20 12:57:00 -0700

    adding a whois cmd/

---
+++ cmd/whois/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/jb0n/gobacnet"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Printf("usage %s <network interface>\n", os.Args[0])
+		os.Exit(1)
+	}
+	iface := os.Args[1]
+
+	cli, err := gobacnet.NewClient(iface, int(gobacnet.DefaultPort))
+	if err != nil {
+		log.Fatalf("error calling gobacnet.NewClient. err=%s", err)
+	}
+	devs, err := cli.WhoIs(-1, -1)
+	if err != nil {
+		log.Fatalf("error calling WhoIs. err=%s", err)
+	}
+	fmt.Printf("found %d devs\n", len(devs))
+	for i := range devs {
+		fmt.Printf("%d: %+v\n", i, devs[i])
+	}
+}
